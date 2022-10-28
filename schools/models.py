86ae1a2054b24d15659d337b24438d3f6503f908
mahdi <mmf1370py@gmail.com>
2022-10-28 13:23:10 -0700

    add Student model for schools app

--- schools/models.py
+++ schools/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+
+    MARK_CHOICES = [
+        ('1', 'Good'),
+        ('2', 'Very Good'),
+    ]
+    DEFAULT_DISPLAY = [
+        (None, 'Your String For Display'),
+    ]
+    first_name = models.CharField(max_length=100, choices=DEFAULT_DISPLAY)
+    last_name = models.CharField(max_length=100, choices=DEFAULT_DISPLAY)
+    mark = models.CharField(choices=MARK_CHOICES)
