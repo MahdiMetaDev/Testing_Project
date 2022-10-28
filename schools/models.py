de9f3aa0079481bdff94dab1081849a171b4fbc0
mahdi <mmf1370py@gmail.com>
2022-10-28 13:58:35 -0700

    add some configuration

--- schools/models.py
+++ schools/models.py
@@ -4,12 +4,15 @@ from django.db import models
 class Student(models.Model):
 
     MARK_CHOICES = [
-        ('1', 'Good'),
-        ('2', 'Very Good'),
-    ]
-    DEFAULT_DISPLAY = [
         (None, 'Your String For Display'),
+        ('1', 'Bad'),
+        ('2', 'Good'),
+        ('3', 'Very Good'),
     ]
-    first_name = models.CharField(max_length=100, choices=DEFAULT_DISPLAY)
-    last_name = models.CharField(max_length=100, choices=DEFAULT_DISPLAY)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     mark = models.CharField(max_length=50, choices=MARK_CHOICES)
+
+    def __str__(self):
+        return self.first_name
--- schools/views.py
+++ schools/views.py
@@ -5,4 +5,4 @@ from .models import Student
 
 def students_list_view(request):
     students = Student.objects.all()
-    render(request, 'schools/students_list.html', context={'students': students})
+    return render(request, 'schools/students_list.html', context={'students': students})
